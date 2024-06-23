38e75a533225feaac112a06ac76703eb69a102c1
wcxto <163146366+wcxto@users.noreply.github.com>
2024-06-23 13:35:20 +0300

    Update 2 Ex_2.py

--- Ex_2.py
+++ Ex_2.py
@@ -9,3 +9,17 @@ def divide(x, y):
         remainder -= y
         quotient += 1
     return quotient
+
+
+# Решение 2
+
+def divide (x, y):
+    result, power = 0, 32
+    y_power = y << power
+    while x >= y:
+        while y_power > x:
+            y_power >>= 1
+            power -= 1
+        result += 1 << power
+        x -= y_power
+    return result 
