6e96516fd84b598f7d8b6151318f8748062383fd
wcxto <163146366+wcxto@users.noreply.github.com>
2024-07-05 16:20:34 +0300

    Update Ex_3.py

--- Ex_3.py
+++ Ex_3.py
@@ -12,3 +12,28 @@ def find_st(text, st):
         if j == m:
             return i
     return -1
+
+
+# Временная сложность этого алгоритма
+
+import functools
+
+def rabin_karp(text, st):
+    if len(st) > len(text):
+        return -1 
+    BASE = 33
+    text_hash = functools.reduce(lambda h, c: h * BASE + ord(c), text[:len(st)], 0)
+    st_hash = functools.reduce(lambda h, c: h * BASE + ord(c), st, 0)
+    power_st = BASE**max(len(st) - 1, 0) 
+    for i in range(len(st), len(text)):
+        if text_hash == st_hash and text[i - len(st):i] == st:
+            return i - len(st) 
+        text_hash -= ord(text[i - len(st)]) * power_st
+        text_hash = text_hash * BASE + ord(text[i])
+    if text_hash == st_hash and text[-len(st):] == st:
+        return len(text) - len(st)
+    return -1 
+
+text = "В роще-чаще рыщет ящер, ищет пищи подходящей"
+st = "ще"
+print(rabin_karp(text, st))
